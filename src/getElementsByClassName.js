de570a682225d281fa0d701edc89074954cef869
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-10-15 10:47:20 -0700

    Passed the minimum requirements for Recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+// This function returns an array-like object of all child elements which
+// have all of the given class names. When called on the document object,
+// the complete document is searched, including the root node.
+// You may also call getElementsByClassName() on any element;
+// it will return only elements which are descendants of the specified
+// root element with the given class names.
+var getElementsByClassName = function(targetClassName) {
+  var results = [];
+  function checkNode(node) {
+    if (node.className) {
+      var classList = node.className;
+      classList = classList.split(' ');
+      for (var i = 0; i < classList.length; i++) {
+        if (classList[i] === targetClassName) {
+          results.push(node);
+        }
+      }
+    }
+    var children = node.childNodes;
+    //children.forEach(checkNode());
+    for (let child of children) {
+      checkNode(child);
+    }
+
+  }
+  checkNode(document.body);
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,54 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  result = '';
+  //function parseString takes in a single element and converts it to a string
+  var parseString = function(arg) {
+    if (typeof arg === 'number') {
+      result += arg;
+    } else if (typeof arg === 'boolean') {
+      if (arg === false) {
+        result += 'false';
+      } else result += 'true';
+    } else if (Array.isArray(arg)) {
+      result += '[';
+      for (var i = 0; i < arg.length; i++) {
+        parseString(arg[i]);
+        result += ',';
+      }
+      if (arg.length > 0) {
+        result = result.substring(0, result.length-1);
+      }
+      result += ']';
+    } else if (typeof arg === 'object') {
+      cleanObj(arg);
+      if (arg === null) {
+        result += 'null';
+      } else {
+        result += '{';
+        for (let key in arg) {
+          result += '\"' + key + '\":';
+          parseString(arg[key]);
+          result += ',';
+        }
+        if (Object.keys(arg).length > 0) {
+          result = result.substring(0, result.length-1);
+        }
+        result += '}';
+      }
+    } else if (typeof arg === 'string') {
+      result += '\"' + arg + '\"';
+    }
+  }
+  var cleanObj = function(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] === 'undefined' || typeof obj[key] === 'function') {
+        delete obj[key];
+      }
+    }
+    return obj;
+  }
+  //call parseString for each element in obj
+  parseString(obj);
+  return result;
 };
